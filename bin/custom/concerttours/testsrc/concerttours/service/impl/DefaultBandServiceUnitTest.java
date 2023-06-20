0d2175089afce7424e8be3c2d6fce6cde830a989
Alena Stankevich <stankevich.lenkaa@mail.ru>
2023-06-20 13:07:03 +0300

    Localized attribute values using impex

--- bin/custom/concerttours/testsrc/concerttours/service/impl/DefaultBandServiceUnitTest.java
+++ bin/custom/concerttours/testsrc/concerttours/service/impl/DefaultBandServiceUnitTest.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -64,7 +65,7 @@ public class DefaultBandServiceUnitTest {
         bandModel.setCode(BAND_CODE);
         bandModel.setName(BAND_NAME);
         bandModel.setAlbumSales(1000L);
-        bandModel.setHistory(BAND_HISTORY);
+        bandModel.setHistory(BAND_HISTORY, Locale.ENGLISH);
     }
 
     /**
